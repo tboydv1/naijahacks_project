97d3e9d0f7aa969e2de5651f9788c7231bd1eaff
tboydv1 <tboydv1@gmail.com>
2020-09-24 20:16:33 +0100

    backup

---
+++ hackhouse/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .import views
+
+urlpatterns = [
+    path('', views.index, name = "index"),
+    path('register', views.register, name="register"),
+    path('successful', views.success, name="suceess"),
+    path('login', views.user_login, name='login'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
+    path('home', views.home, name='home')
+
+]
